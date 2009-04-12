c350d4ebfd71c9e1ce384f3b394c39b026853b49
koroglu.ozan <koroglu.ozan@c266ead0-05ae-11de-a1f0-819f45317607>
2009-04-12 11:20:23 +0000

    refactored to use objects

--- manageStructure.php
+++ manageStructure.php
@@ -37,28 +37,8 @@ switch($action)
 			$ownerEl = (int) checkVariable($_POST['ownerEl']);
 			$slave = (int) checkVariable($_POST['slave']);
 			$name = checkVariable($_POST['name']);			
-	
-			$sql = sprintf('INSERT INTO ' 
-								. TREE_TABLE_PREFIX . '_elements(name, position, ownerEl, slave)
-							SELECT 
-								\'%s\', ifnull(max(el.position)+1, 0), %d, %d 
-							FROM '
-								. TREE_TABLE_PREFIX . '_elements el 
-							WHERE 
-								el.ownerEl = %d ',
-							$name , $ownerEl, $slave, $ownerEl);
-			
-	        if (defined("DEMO_MODE")) 
-			{
-				$insertId = rand(0, 10000);				
-				$out =  '({ "elementId":"'.$insertId.'", "elementName":"'.$name.'", "slave":"'.$slave.'"})';
-			}      
-			else if ($db->query($sql) == true) {
-				$out = '({ "elementId":"'.$db->lastInsertId().'", "elementName":"'.$name.'", "slave":"'.$slave.'"})';
-			}
-			else {
-				$out = FAILED;
-			}
+		  
+			$out = $treeManager->insertElement($name, $ownerEl, $slave);						
 		}
 		else {
 			$out = FAILED; 
@@ -77,29 +57,8 @@ switch($action)
 			$ownerEl = 0;
 		}
 		
-        $sql = sprintf("SELECT 
-        					Id, name, slave 
-        				FROM " 
-        					. TREE_TABLE_PREFIX . "_elements
-		      			WHERE
-		      				ownerEl = %d  
-		      			ORDER BY
-		      				position ",
-        				$ownerEl);
-	  
-		if (defined("DEMO_MODE")) 
-		{
-			$out = "<li class='text' id='14'><span>file-1</span></li><li class='text' id='15'>".
-					"<span>file-2</span></li><li class='text' id='16'><span>file-3</span></li>";
-		}
-		else {			
-        	$out = getElementList($db, $sql, $_SERVER['PHP_SELF']);
-		}
-		
-		if ($out === false) 
-        {	   			
-        	$out = FAILED; 	
-	    }            
+  		$out = $treeManager->getElementList($ownerEl, $_SERVER['PHP_SELF']);
+		         
     }
 	break;		
     case "updateElementName":
@@ -112,25 +71,8 @@ switch($action)
 		{			
 			$name = checkVariable($_POST['name']);
 			$elementId = (int) checkVariable($_POST['elementId']); 			
-			
-        	$sql = sprintf('UPDATE ' 
-        						. TREE_TABLE_PREFIX.'_elements 
-							SET 
-								name = \'%s\'
-					    	WHERE 
-					    		Id = %d ',
-        					$name, $elementId);
-        					
-		    if (defined("DEMO_MODE")) 
-		    {
-		    	$out = '({"elementName":"'.$name.'", "elementId":"'.$elementId.'"})';
-		    }
-			else if ($db->query($sql) == true) {
-				$out = '({"elementName":"'.$name.'", "elementId":"'.$elementId.'"})';
-			}
-			else {
-				$out = FAILED;
-			}					
+		
+			$out = $treeManager->updateElementName($name, $elementId);
 		}                         
 		else {
 			$out = FAILED;	
@@ -146,18 +88,10 @@ switch($action)
 		if (isset($_POST['elementId']) && !empty($_POST['elementId']))
 		{
         	$elementId = (int) checkVariable($_POST['elementId']);	 
-        	              
-			if (defined("DEMO_MODE")) 
-			{
-			 	$out = SUCCESS;	
-			}        	
-			else if (deleteData($db, $elementId) === true) {				
-				$out = SUCCESS; 
-			}
-			else {
-				$out = FAILED;
-			}                
-        }
+        	 
+			 
+			$out = $treeManager->deleteElement($elementId);             
+	    }
         else {
 			$out = FAILED;	
 		}
@@ -178,66 +112,10 @@ switch($action)
 			$destOwnerEl = (int) checkVariable($_POST['destOwnerEl']);
 			$position = (int) checkVariable($_POST['position']);
 			
-			$sql = sprintf('SELECT
-						 		ownerEl, position 
-							FROM '
-								. TREE_TABLE_PREFIX . '_elements 
-							WHERE 
-								Id = %d
-							LIMIT 1',
-							$elementId);
-			if (defined("DEMO_MODE")) 
-			{
-			 	$out = SUCCESS;	
-			}  			
-			else if ($result = $db->query($sql))
-			{			
-				if ($element = $db->fetchObject($result))
-				{						
-					$sql1 = sprintf('UPDATE '
-										 . TREE_TABLE_PREFIX . '_elements 
-									 SET 
-									 	position = position - 1
-									 WHERE  
-									 	position > %d
-									    AND
-									    ownerEl = %d ',
-									 $element->position, $element->ownerEl);
-							   
-					$sql2 = sprintf('UPDATE '
-										. TREE_TABLE_PREFIX . '_elements 
-									 SET 
-									 	position = position + 1
-									 WHERE
-							 			 position >= %d 
-									   	 AND
-									   	 ownerEl = %d ',
-									 $position, $destOwnerEl);
-							   
-					$sql3 = sprintf('UPDATE '
-										. TREE_TABLE_PREFIX . '_elements 
-									 SET 
-									 	position = %d , ownerEl = %d
-									 WHERE 
-									 	Id = %d ',
-										$position, $destOwnerEl, $elementId);
-	
-					
-					if ($db->query($sql1) && $db->query($sql2) && $db->query($sql3)) {					
-						$out = SUCCESS;
-					}
-					else {						
-						$out = FAILED;
-					}		
-				}
-				else{
-					$out = FAILED;
-				}	
-			}
-			else{
-				$out = FAILED;
-			}		
-		}
+		
+			$out = $treeManager->changeOrder($elementId, $destOwnerEl, $position);
+			
+		}			
 		else{		
 			$out = FAILED;
 		}			
