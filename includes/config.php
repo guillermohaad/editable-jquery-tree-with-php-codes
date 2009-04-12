ea769b22e4c88011b6610be53b5b43349ca64386
koroglu.ozan <koroglu.ozan@c266ead0-05ae-11de-a1f0-819f45317607>
2009-04-12 11:14:09 +0000

    TARGET_PLATFORM, DATABASE_PLATFORM, FILE_SYSTEM_PLATFORM macros have been defined

--- includes/config.php
+++ includes/config.php
@@ -16,17 +16,32 @@ $dbHost = "localhost";
 // database user name
 $dbUsername = "root";
 // password to connect to database
-$dbPassword = "21236161";
+$dbPassword = "BlindS";
 // database name
-$dbName = "eyillik_demo";
+$dbName = "php";
 
 define ("TREE_TABLE_PREFIX", "tree");
 
-// if you want to run it in demo mode, enable the macro below...
-//define("DEMO_MODE", true);
+/** target platform types
+ *  this script runs both database and file system 
+ *  database platform only supports mysql
+ */
+define ("DATABASE_PLATFORM", 0); // Don't edit
+define ("FILE_SYSTEM_PLATFORM", 1); // Don't edit
 
 define ("FAILED", "-1");  // Don't edit
 define ("SUCCESS", "1");  // Don't edit
+
+
+// choose target platform according to your needs 
+// it may be DB_PLATFORM or FILE_SYSTEM_PLATFORM
+define ("TARGET_PLATFORM", DATABASE_PLATFORM);
+
+// if you want to run it in demo mode, enable the macro below
+// if this macro is enabled, it runs in demo mode regardsless of whatever TARGET_PLATFORM is
+//define("DEMO_MODE", true);
+
+
 
 error_reporting(0);
 
