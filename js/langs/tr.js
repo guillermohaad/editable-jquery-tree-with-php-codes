55601e53503b5acd0235bc37ee372c09cfd92ca0
ahmetmermerkaya <ahmetmermerkaya@gmail.com>
2009-04-26 11:48:49 +0000

    "error" tanımı "error_1" olarak değiştirildi. Dosya sistemi kullanırken aynı isme sahip elemanın bulunması ile ilgili bir hata oluştuğunda kullanıcı bilgilendirmek için "error_2" tanımı eklendi.

--- js/langs/tr.js
+++ js/langs/tr.js
@@ -1,7 +1,8 @@
 // JavaScript Document
 
 langManager.addIndexes({
-error:'\u0130\u015Flemde bir hata olu\u015Ftu.',
+error_1:'\u0130\u015Flemde bir hata olu\u015Ftu.',
+error_2:'Ayn\u0131 isme sahip eleman zaten bulunmaktadır.',
 willReload:'Sayfa yeniden y\u00FCklenecek.',
 deleteConfirm:'Bu eleman\u0131 silmek istedi\u011Finize eminmisiniz?',
 doOneOperationAtATime:'Herhangi bir i\u015Flem yap\u0131l\u0131rken ikinci farkl\u0131 bir i\u015Flem yap\u0131lamaz.',
@@ -11,9 +12,5 @@ addDocMenu:'Dosya ekle',
 addFolderMenu:'Klas\u00F6r ekle',
 editMenu:'D\u00FCzenle',
 deleteMenu:'Sil',
-//dataSaved:'\u0130\u015Flem ba\u015Far\u0131yla tamamland\u0131.',
 missionCompleted:'\u0130\u015Flem ba\u015Far\u0131yla tamamland\u0131.',
-//nameChanged:'\u0130\u015Flem ba\u015Far\u0131yla tamamland\u0131.',
-//newItemAdded:'\u0130\u015Flem ba\u015Far\u0131yla tamamland\u0131.',
-noActiveElement:'Aktif dosya bulunmamaktad\u0131r.'
 });
