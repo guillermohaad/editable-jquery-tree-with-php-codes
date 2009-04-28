fd0d76edb1ca828122875ab617fadc27a4be121f
ahmetmermerkaya@gmail.com <ahmetmermerkaya@gmail.com@c266ead0-05ae-11de-a1f0-819f45317607>
2009-04-28 19:48:28 +0000

    Code format is justified.

--- trunk/common.php
+++ trunk/common.php
@@ -15,24 +15,19 @@ $db = NULL;
 $treeManager = NULL;
 if (defined("DEMO_MODE") === true && DEMO_MODE === true)
 {
-	require_once('includes/classes/DemoTreeManager.php');
-		
+	require_once('includes/classes/DemoTreeManager.php');		
 	$treeManager = new DemoTreeManager(null);
 }
 else if (TARGET_PLATFORM == DATABASE_PLATFORM ) 
 {
 	require_once('includes/classes/Mysql.php');
 	require_once('includes/classes/DBTreeManager.php');
-
 	$db = new MySQL($dbHost, $dbUsername, $dbPassword, $dbName);	
 	$treeManager = new DBTreeManager($db);
 }
-else if (TARGET_PLATFORM == FILE_SYSTEM_PLATFORM) {
-	
+else if (TARGET_PLATFORM == FILE_SYSTEM_PLATFORM) 
+{	
 	require_once("includes/classes/FileTreeManager.php");
 	$treeManager = new FileTreeManager(FILE_ROOT);
-	
 }
-
-
 ?>
