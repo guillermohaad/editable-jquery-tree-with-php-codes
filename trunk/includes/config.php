b30c99ba32fbfec7e193245d2a3be387a9138a45
ahmetmermerkaya@gmail.com <ahmetmermerkaya@gmail.com@c266ead0-05ae-11de-a1f0-819f45317607>
2009-04-28 19:39:48 +0000

    Comment added; TARGET_PLATFORM initializations are made separately;

--- trunk/includes/config.php
+++ trunk/includes/config.php
@@ -11,41 +11,63 @@ if (!defined("IN_PHP"))
 	die();
 }
 
-// database host
-$dbHost = "localhost";   
-// database user name
-$dbUsername = "root";
-// password to connect to database
-$dbPassword = "21236161";
-// database name
-$dbName = "php";
+/** 
+ * if you want to run in demo mode, enable the macro below
+ * if it runs in demo mode, it doesn't care what TARGET_PLATFORM is
+ */
+//define("DEMO_MODE", true);
 
-define ("TREE_TABLE_PREFIX", "tree");
 
 /** target platform types
- *  this script runs both database and file system. 
- *  database platform only supports mysql
+ *  this script runs for both database(mysql) and file system. 
+ *  database is supposed to be mysql
  */
 define ("DATABASE_PLATFORM", 0); // Don't edit
 define ("FILE_SYSTEM_PLATFORM", 1); // Don't edit
 
+
+/** 
+ * choose target platform according to your needs 
+ * it may be DATABASE_PLATFORM or FILE_SYSTEM_PLATFORM
+ */
+define ("TARGET_PLATFORM", FILE_SYSTEM_PLATFORM);
+
+
+if (TARGET_PLATFORM == FILE_SYSTEM_PLATFORM)
+{
+	/**
+ 	* if FILE_SYSTEM_PLATFORM is selected as TARGET_PLATFORM,
+ 	* then assign root folder adress to FILE_ROOT macro
+ 	*/ 
+	define("FILE_ROOT", "C:\Documents and Settings\aomermerkaya\My Documents\MapTracker"); 
+}
+else if (TARGET_PLATFORM == DATABASE_PLATFORM)
+{
+	/**
+	 * if DATABASE_PLATFORM is selected as TARGET_PLATFORM,
+	 * set the variables to connect database and 
+	 * set the TREE_TABLE_PREFIX if you want to
+	 * change the prefix of table name
+	 */	
+	// set database host 
+	$dbHost = "localhost";
+	//set database user name    
+	$dbUsername = "root"; 
+	// set password to connect to database	 
+	$dbPassword = "21236161";
+	// set database name
+	$dbName = "test";
+	// set database table prefix 
+	define ("TREE_TABLE_PREFIX", "tree");
+}
+else {
+	die("No known target platform specified, in includes/config.php");
+}
+
+
 define ("SUCCESS", 1);  // Don't edit
 define ("FAILED", -1);  // Don't edit
-define ("FAILED_FILE_WITH_SAME_NAME_EXIST", -2); // Don't edit
+define ("FAILED_FILE_WITH_SAME_NAME_EXIST", -2); // Don't edit, it is only used in FILE_SYSTEM_PLATFORM
 
-
-
-// choose target platform according to your needs 
-// it may be DB_PLATFORM or FILE_SYSTEM_PLATFORM
-define ("TARGET_PLATFORM", DATABASE_PLATFORM);
-
-// ATTENTION,  if FILE_SYSTEM_PLATFORM is selected then assign root folder adress to this macro
-define("FILE_ROOT", "/home/mekya/public_html/test");
-
-// if you want to run it in demo mode, enable the macro below
-// if this macro is enabled, it runs in demo mode regardsless of whatever TARGET_PLATFORM is
-//define("DEMO_MODE", true);
-
-error_reporting(0);
-
+error_reporting(0); 
 ?>
